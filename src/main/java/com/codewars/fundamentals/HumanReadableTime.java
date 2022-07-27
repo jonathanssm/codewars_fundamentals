3993f29d3245526efa4b430a3b2fc567bf0bc245
jonathanssm <jonathan.ssm@hotmail.com>
2022-07-27 13:59:48 -0300

    [DEV-1] - Implementing HumanReadableTime Training

---
+++ src/main/java/com/codewars/fundamentals/HumanReadableTime.java
@@ -0,0 +1,25 @@
+package com.codewars.fundamentals;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class HumanReadableTime {
+    public static String makeReadable(int seconds) {
+        if (seconds <= 359999) {
+            int hours = seconds / 3600;
+            int minutes = (seconds % 3600) / 60;
+            seconds = seconds % 60;
+
+            return dateFormat(hours, minutes, seconds);
+        }
+
+        return dateFormat(0, 0, 0);
+    }
+
+    private static String dateFormat(int hours, int minutes, int seconds) {
+        return String.format("%s:%s:%s", putLeftPad(hours), putLeftPad(minutes), putLeftPad(seconds));
+    }
+
+    private static String putLeftPad(int number) {
+        return StringUtils.leftPad(String.valueOf(number), 2, "0");
+    }
+}
